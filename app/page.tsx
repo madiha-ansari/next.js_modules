5bffd9fd7121de55caafebae3cd2406958dda5e0
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-10 17:42:14 +0500

    inline styling

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <h1 style={{ color: 'blue', background: 'pink' }}><center>Hello world</center></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat eveniet molestiae, maiores quo ullam optio ut mollitia magnam a, voluptatum ipsum aliquam aperiam repellat iure? Nihil cum neque quisquam. Itaque!</p>
+    </div>
+  )
+}
+
+export default Page
+ 
